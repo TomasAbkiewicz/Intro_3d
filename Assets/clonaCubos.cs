d37aeb2a20f138efdd35a906898d8380817ef5ef
TomasAbkiewicz <166508961+TomasAbkiewicz@users.noreply.github.com>
2025-05-16 10:39:15 -0300

    clonacubos

---
+++ Assets/clonaCubos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clonaCubos : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ClonarCubos()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
